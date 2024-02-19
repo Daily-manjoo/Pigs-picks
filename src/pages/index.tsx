071132bc15a813a82b97c138a8b365dc12f82957
Daily-manjoo <yuslz35@gmail.com>
2024-02-20 00:45:17 +0900

    Marker, Map 컴포넌트 분리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Map from "@/components/Map";
+import Markers from "@/components/Markers";
+import * as stores from "@/data/store_data.json";
+import { MarkerProps } from "@/components/Markers";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [map, setMap] = useState(null);
+  const storeDatas = stores["DATA"];
   return (
     <>
-    <Map />
+    <Map setMap={setMap} />
+    <Markers storeDatas={storeDatas} map={map} />
     </>
   );
 }
