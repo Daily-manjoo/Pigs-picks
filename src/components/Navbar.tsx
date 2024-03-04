91d4fd81bddc93a066486e25738e283dafe1f13b
Daily-manjoo <yuslz35@gmail.com>
2024-03-05 00:00:37 +0900

    google 로그인 기능 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 import { useState } from "react"
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
+import { useSession, signOut } from "next-auth/react";
 
 export default function NavBar(){
     const [isOpen, setIsOpen] = useState(false);
+    const {data, status} = useSession();
+
     return(
         <>
             <div className="navbar">
@@ -19,9 +22,14 @@ export default function NavBar(){
                     <Link href="/stores/likes" className="navbar__list--item">
                         찜한 가게
                     </Link>
+                    {status === 'authenticated' ? 
+                    <button type="button" onClick={() => signOut()}>
+                        로그아웃
+                    </button> : 
                     <Link href="/api/auth/signin" className="navbar__list--item">
                         로그인
-                    </Link>
+                    </Link>}
+                    
                 </div>
                 {/*Mobile button */}
                 <div role="presentation" className="navbar__button" onClick={() => setIsOpen((val) => !val)}>
