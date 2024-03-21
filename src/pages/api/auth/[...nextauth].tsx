65ad7e94fd71ed1280b81f0874b4a01dba1f7257
Daily-manjoo <yuslz35@gmail.com>
2024-03-22 00:41:15 +0900

    next auth 재설정

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -8,6 +8,11 @@ import KakaoProvider from "next-auth/providers/kakao"
 const prisma = new PrismaClient();
 
 export const authOptions = {
+    session: {
+        strategy: 'jwt' as const, //로그인 이후에도 middleware.ts의 경로 접근하기 위해 명시
+        maxAge: 60 * 60 * 24, //세션 최대 수명(초단위 표시, 24시간 유지)
+        updateAge: 60 * 60 * 2, //세션 업데이트 주기(초단위 표시, 2시간 유지 )
+    },
     adapter: PrismaAdapter(prisma),
     //Configure one or more authentication providers
     providers: [
