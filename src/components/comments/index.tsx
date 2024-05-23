4a41864f7885f36f556a0dbb4a9af8b1212fc7d8
Daily-manjoo <yuslz35@gmail.com>
2024-05-23 22:43:55 +0900

    댓글창 스타일링

--- src/components/comments/index.tsx
+++ src/components/comments/index.tsx
@@ -14,22 +14,29 @@ export default function Comments({ storeId }: CommentProps) {
   return (
     <div className="md:max-w-2xl px-2 py-8 mb-20 mx-auto">
       {/*comment form*/}
-      <div className="flex flex-col space-y-4">
-        <form
-          onSubmit={handleSubmit(async (data) => {
-            console.log(data);
-          })}
+
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          console.log(data);
+        })}
+        className="flex flex-col space-y-2"
+      >
+        {errors?.body?.type === "required" && (
+          <div className="text-xs text-red-500">필수로 입력해주세요.</div>
+        )}
+        <textarea
+          rows={3}
+          placeholder="댓글을 입력해주세요!"
+          {...register("body", { required: true })}
+          className="w-full block min-h-[120px] resize-none rounded-md border bg-transparent py-2 px-4 text-black placeholder:text-gray-400 text-sm leading-6"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 text-sm font-semibold mt-2 rounded-md shadow-sm "
         >
-          <textarea
-            rows={3}
-            placeholder="댓글을 입력해주세요!"
-            {...register("body", { required: true })}
-            className="w-full block min-h-[120px] resize-none border-0 bg-transparent py-2 px-4 text-black placeholder:text-gray-400 text-sm leading-6"
-          />
-        </form>
-        {/*error box*/}
-        <div className="text-xs text-red-500">필수로 입력해주세요.</div>
-      </div>
+          작성하기
+        </button>
+      </form>
       {/*comment list*/}
     </div>
   );
