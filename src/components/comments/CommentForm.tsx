522e92b20868accba68681ab03d61d93462fd1a9
Daily-manjoo <yuslz35@gmail.com>
2024-05-27 23:52:03 +0900

    Comment Form 컴포넌트 분리

---
+++ src/components/comments/CommentForm.tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+
+interface CommentFormProps {
+  storeId: number;
+}
+
+export default function CommentForm({ storeId }: CommentFormProps) {
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    formState: { errors },
+  } = useForm();
+  return (
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        const result = await axios.post("/api/comments", {
+          ...data,
+          storeId, //body값은 파라미터로
+        });
+
+        if (result.status === 2000) {
+          toast.success("댓글을 등록했습니다.");
+          resetField("body"); //바디값 리셋
+        } else {
+          toast.error("다시 시도해주세요.");
+        }
+      })}
+      className="flex flex-col space-y-2"
+    >
+      {errors?.body?.type === "required" && (
+        <div className="text-xs text-red-500">필수로 입력해주세요.</div>
+      )}
+      <textarea
+        rows={3}
+        placeholder="댓글을 입력해주세요!"
+        {...register("body", { required: true })}
+        className="w-full block min-h-[120px] resize-none rounded-md border bg-transparent py-2 px-4 text-black placeholder:text-gray-400 text-sm leading-6"
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 text-sm font-semibold mt-2 rounded-md shadow-sm "
+      >
+        작성하기
+      </button>
+    </form>
+  );
+}
--- src/components/comments/index.tsx
+++ src/components/comments/index.tsx
@@ -1,7 +1,5 @@
-import axios from "axios";
-import { useForm } from "react-hook-form";
 import { useSession } from "next-auth/react";
-import { toast } from "react-toastify";
+import CommentForm from "./CommentForm";
 
 interface CommentProps {
   storeId: number;
@@ -9,48 +7,10 @@ interface CommentProps {
 
 export default function Comments({ storeId }: CommentProps) {
   const { status } = useSession();
-  const {
-    register,
-    handleSubmit,
-    resetField,
-    formState: { errors },
-  } = useForm();
   return (
     <div className="md:max-w-2xl px-2 py-8 mb-20 mx-auto">
       {/*comment form*/}
-      {status === "authenticated" && (
-        <form
-          onSubmit={handleSubmit(async (data) => {
-            const result = await axios.post("/api/comments", {
-              ...data,
-              storeId, //body값은 파라미터로
-            });
-
-            console.log(result);
-
-            if (result.status === 2000) {
-              toast.success("댓글을 등록했습니다.");
-            }
-          })}
-          className="flex flex-col space-y-2"
-        >
-          {errors?.body?.type === "required" && (
-            <div className="text-xs text-red-500">필수로 입력해주세요.</div>
-          )}
-          <textarea
-            rows={3}
-            placeholder="댓글을 입력해주세요!"
-            {...register("body", { required: true })}
-            className="w-full block min-h-[120px] resize-none rounded-md border bg-transparent py-2 px-4 text-black placeholder:text-gray-400 text-sm leading-6"
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 text-sm font-semibold mt-2 rounded-md shadow-sm "
-          >
-            작성하기
-          </button>
-        </form>
-      )}
+      {status === "authenticated" && <CommentForm storeId={storeId} />}
 
       {/*comment list*/}
     </div>
