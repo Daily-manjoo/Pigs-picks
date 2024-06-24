e71a1b59023670bc42cc7e41f60a8b986240a721
Daily-manjoo <yuslz35@gmail.com>
2024-06-24 22:13:47 +0900

    tailwind.config.js 경로 변경(스타일 문제 해결)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
